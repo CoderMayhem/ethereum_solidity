ab9d911fa218d45f6235216594cb4167d1f88147
CoderMayhem <namanmishra1900@gmail.com>
2021-06-27 03:44:00 +0530

    mapping example1

---
+++ mappingExamples.sol
@@ -0,0 +1,43 @@
+pragma solidity ^0.8.4;
+
+contract MappingStructExample{
+    
+    struct Payment{
+        uint amount;
+        uint timestamp;
+    }
+    
+    struct Balance{
+        uint totalBalance;
+        uint numPayments;
+        mapping(uint=>Payment) payments;
+    }
+    
+    mapping(address=>Balance) public balanceRecieved;
+    
+    
+    function getBalance() public view returns(uint){
+        return address(this).balance;
+    }
+    
+    function sendMoney() public payable{
+        balanceRecieved[msg.sender].totalBalance += msg.value;
+        
+        Payment memory payment = Payment(msg.value, block.timestamp);
+        balanceRecieved[msg.sender].payments[balanceRecieved[msg.sender].numPayments] = payment;
+        balanceRecieved[msg.sender].numPayments++;
+    }
+    
+    function withdrawMoney(address payable _to, uint amount) public{
+        //checks if amount is less than the value held by the sender
+        require(amount<=balanceRecieved[msg.sender].totalBalance, "you don't have enough funds for carrying out the transaction");
+        balanceRecieved[msg.sender].totalBalance -= _amount;
+        _to.transfer(_amount);
+    }
+    
+    function withdrawAllMoney(address payable _to) public {
+        uint balanceToSend = balanceRecieved[msg.sender].totalBalance; 
+        balanceRecieved[msg.sender].totalBalance = 0;
+        _to.transfer(balanceToSend);
+    }
+}
